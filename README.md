cb72339e31d1bab8ef2d103d777a427ffa5a0b25
Ashish <ashishmule946@gmail.com>
2022-11-29 23:44:01 +0530

    removed fake word

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# Mock-API
+# Open-API
 
-Free fake REST API and GRAPHQL for testing and prototyping. 
+Free REST API and GRAPHQL for testing and prototyping. 
 
 Live Demo : https://open-data.herokuapp.com/
 
 
 # Open source project
 
-https://open-data.herokuapp.com/ is a free online REST API as well as GRAPHQL that you can use whenever you need some fake data. This site is for study and practice purpose only, All data in this site is fake does not belong to anyone.
-
-The credit for this website design goes to Floriannicolas and CoderX707 for the backend functionality. Thanks for the fake data www.mockaroo.com.
+https://open-data.herokuapp.com/ is a free online REST API as well as GRAPHQL that you can use whenever you need some data. This site is for study and practice purpose only, All data in this site is does not belong to anyone.
 
 ## Contribute
 Pull requests are welcome, or open up an issue if you have ideas for additional functionality, new features or bugs.
--- src/doc/public/js/helpers/get-started.js
+++ src/doc/public/js/helpers/get-started.js
@@ -6,12 +6,12 @@ function getStartedContent(id) {
     https://open-data.herokuapp.com/graphql/v1/
     </pre>
     <p>
-        Free fake <strong>REST API</strong> and <strong>GRAPHQL</strong> for testing and prototyping. 
+        Free <strong>REST API</strong> and <strong>GRAPHQL</strong> for testing and prototyping. 
     </p>  
     <p>
-    https://open-data.herokuapp.com/ is a free online <strong>REST API</strong> as well as <strong>GRAPHQL</strong> that you can use whenever you need some fake data. It can be in a README on GitHub, simply to test things locally.
+    https://open-data.herokuapp.com/ is a free online <strong>REST API</strong> as well as <strong>GRAPHQL</strong> that you can use whenever you need some data. It can be in a README on GitHub, simply to test things locally.
     This site is for study and practice purpose only,
-    All data in this site is fake does not belong to anyone
+    All data in this site is does not belong to anyone
     </p>   
     <p><strong>Pull requests are welcome, or open up an issue if you have ideas for additional functionality, new features or bugs.</strong> On the right side, we have provided a GitHub link.</p>
     </div>`);
